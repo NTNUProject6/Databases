ec4faf109d46d99605a444f8e6c8c79d80fa22a5
keangping <Amonveschayakul@dhcp-045046.wlan.ntnu.no>
2014-10-21 15:12:41 +0200

    Initial commit.

---
+++ src/cabiNet/Database.java
@@ -0,0 +1,91 @@
+package cabiNet;
+import java.sql.Date;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import com.mysql.jdbc.Connection;
+import com.mysql.jdbc.Statement;
+
+public class Database {
+	
+	private static java.sql.Connection connection;
+	private static java.sql.Statement statement;
+	
+	private static String url = "jdbc:mysql://mysql.stud.ntnu.no/nuttawua_cabinInfo",
+					username = "nuttawua_cabinet", password = "carbinet";
+			
+	public Database(){
+		
+		try {
+			connection = DriverManager.getConnection(url, username, password);
+			System.out.print("success");
+			
+		} catch(SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void addBooking(Booking b){
+		try{
+			statement = connection.createStatement();
+			String query = "insert into booking (booking_id, user_id, cabin_id, date_from, date_to)" 
+							+ " values (?, ?, ?, ?, ?)";
+			PreparedStatement pStatement = connection.prepareStatement(query); 
+			pStatement.setInt(1, 10);
+			pStatement.setString(2, b.getUser_id());
+			pStatement.setInt(3, b.getCabin_id());
+			pStatement.setDate(4, b.getDate_From());
+			pStatement.setDate(5, b.getDate_To());
+			
+			pStatement.execute();
+			
+			statement.close();
+			connection.close();
+		} catch (Exception e){
+			System.err.println("Got an exception!");
+		    System.err.println(e.getMessage());
+		}
+	}
+	
+	public ArrayList<Booking> getBookingFromCabin1(){
+		ArrayList<Booking> ab = new ArrayList<Booking>();
+		try{
+			statement = connection.createStatement();
+			String sql = "select * from booking";
+			ResultSet rs = statement.executeQuery(sql);
+			while(rs.next()){
+				Booking b = new Booking();
+				b.setId(rs.getInt("booking_id"));
+				b.setUser_id(rs.getString("user_id"));
+				b.setCabin_id(rs.getInt("cabin_id"));
+				b.setDate_From(rs.getDate("date_from"));
+				b.setDate_To(rs.getDate("date_to"));
+				ab.add(b);
+			}
+			rs.close();
+//			statement.close();
+//			connection.close();
+		} catch(SQLException se){
+			se.printStackTrace();
+		}
+		return ab;
+	}
+	
+	public static void main(String[] args){
+		Database db = new Database();
+		ArrayList<Booking> ab = new ArrayList<Booking>();
+		ab = db.getBookingFromCabin1();
+		for(Booking b : ab){
+			System.out.println("////////////////////////////////");
+			System.out.println("booking_id:	" + b.getId());
+			System.out.println("user_id:	" + b.getUser_id());
+			System.out.println("cabin_id:	" + b.getCabin_id());
+			System.out.println("date_from:	" + b.getDate_From());
+			System.out.println("date_to:	" + b.getDate_To());
+		}
+		db.addBooking(ab.get(0));
+	}
+}
